42781b1774b92431d0de337714ab9a7ab041f4d1
Oscar Miranda <ommiranda@gmail.com>
2023-01-10 15:32:50 -0500

    avance 01

--- codigo.js
+++ codigo.js
@@ -1,16 +1,108 @@
 
-/* dineroCofla = prompt("Cuanto dinero tienes?");
-dineroRoberto = prompt("Cuanto dinero tienes?");
-dineroPedro = prompt("Cuanto dinero tienes?");
 
- */
 
-function saludar(){
-    alert("Hola!");
-    return "La funcion se ejecuto correctamente";
+/* array1 = ["Oscar", "Jose", "Manuel","Luis"];
+
+array2 = ["Marcela", "Caty", array1, "Sam"];
+
+
+condicion01:
+for(let array in array2){
+    if (array == 3) {
+        for(let array of array1){
+            break condicion01;
+            document.write(array + "<br>");
+
+           
+            }
+        }
+    else{
+        document.write(array2[array] +"<br>");
+        }
+
+    } */
+
+  /*   function suma(a, b){
+        let numero1 = a;
+        let numero2 = b;
+        let resultado = numero1 + numero2;
+        document.write(resultado);
+        document.write("<br>");
+
+        return(resultado);
     }
 
-let saludo = saludar();
+    
 
-document.write(saludo);
+ let respuesta = suma(12, 30); */
+
+    
+   /*  const respuesta = (a, b)=>{
+        let numero1 = a;
+        let numero2 = b;
+        let resultado = numero1 + numero2;
+        document.write(resultado);
+    }
+    
+   
+   respuesta (12, 30);
+
+    */
+    
+    
+   /*  const suma = (a,b) =>{
+        
+        document.write(a + b);
+    } 
+
+    suma (70,40); */
+
+
+    /* let free = false;
+
+    const validarcliente = (time, edad) =>{
+        // let edad = prompt("Cual es tu edad ?");
+        if (edad> 18) {
+            if (time >= 2 && time <= 7 && free == false){
+                alert("Puedes pasar gratis.")
+                free = true;
+            }
+            lert("Puedes pasar, la entrada es $$$")
+        }
+        else{
+            alert("Eres menor de edad!!!, No hay ingreso para ti.");
+        }
+    }
+
+    validarcliente (3, 16);
+ */
+// --------------------------------------------------------------------------------------------
+
+   /*  let pc1 = {
+        nombre: "HP",
+        procesador: "Intel Core I7",
+        ram: "16GB",
+        espacio: "1TB"
+        }; */
+
+   /*  let pc2 = {
+        nombre: "DELL",
+        procesador: "Intel Core I9",
+        ram: "32GB",
+        espacio: "5TB"
+        }; */
+    
+   /*  let nombre = pc1["nombre"];
+    let procesador = pc1["procesador"];
+    let ram = pc1["ram"];
+    let espacio = pc1["espacio"];
+
+    frase = `La marca de mi PC es ${nombre} <br>
+        El procesador es un ${procesador} <br>
+        La memoria RAM es de ${ram} <br>
+        El espacio de almacenamiento es de ${espacio} <br>`;
+
+    document.write(frase); */
+
+
 
